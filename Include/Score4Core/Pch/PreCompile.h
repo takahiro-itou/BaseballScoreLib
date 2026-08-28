c76eaf4f85138a19ed064e54f8004fcbfd103425
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-28 21:19:55 +0900

    リファクタリング
    (1) プリコンパイル済みヘッダのふりをしたファイルを作成

---
+++ Include/Score4Core/Pch/PreCompile.h
@@ -0,0 +1,36 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Baseball  Score  Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Pre-Compiled Header File.
+**
+**      @file       Pch/PreCompile.h
+**/
+
+#if !defined( BASEBALLSCORE_PCH_INCLUDED_PRE_COMPILE_H )
+#    define   BASEBALLSCORE_PCH_INCLUDED_PRE_COMPILE_H
+
+
+#if !defined( BASEBALLSCORE_SYS_INCLUDED_STDDEF_H )
+#    include    <stddef.h>
+#    define   BASEBALLSCORE_SYS_INCLUDED_STDDEF_H
+#endif
+
+
+#if !defined( BASEBALLSCORE_COMMON_INCLUDED_SCORE4_TYPES )
+#    include    "Score4Core/Common/Score4Types.h"
+#endif
+
+
+#endif
